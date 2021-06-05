3c3e63e3c15f803e056daa3038fb9fab22e351ae
AMITHESH K N <46914484+Amitheshkn@users.noreply.github.com>
2021-06-05 18:08:55 +0530

    Update text_to_handwritten.py

--- text_to_handwritten.py
+++ text_to_handwritten.py
@@ -1,4 +1,6 @@
 import pywhatkit
 pywhatkit.text_to_handwriting(
-    input() , r"C:\Users\ABHI\Documents\handwrittenpyfiles\demo.png" ,rgb=(0, 0, 255)
+    input(),
+    r"C:\Users\ABHI\Documents\handwrittenpyfiles\demo.png",
+    rgb=(0, 0, 255)
 )
